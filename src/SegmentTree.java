2a267715defa06f0d8627fb11b08a12f14ecc372
ShaunTheCode <1958552792@qq.com>
2021-07-09 15:48:43 +0800

    添加线段树

---
+++ src/SegmentTree.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author wxy
+ * @date 2021/07/09 15:47
+ * @since 1.0
+ */
+public class SegmentTree
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hello");
+    }
+
+}
